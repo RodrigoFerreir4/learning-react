55693ad5c59056338b4e45c6cd9a974d1c8c3632
RodrigoF3rr <rodrigo.4dev@gmail.com>
2023-10-05 18:48:27 -0400

    feat: implement dinamic posts

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -4,6 +4,59 @@ import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/RodrigoFerreir4.png",
+      name: "Rodrigof3rr",
+      role: "Web Developer",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content:
+          "Olá pessoal! Acabei de terminar um novo projeto e estou empolgado para compartilhá-lo com vocês.",
+      },
+      {
+        type: "image",
+        content: "https://example.com/imagem_projeto1.jpg",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Neste projeto, utilizei tecnologias como React e Node.js para criar uma aplicação incrível.",
+      },
+    ],
+    publishedAt: new Date("2023-10-10 19:23:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "Web Developer",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content:
+          "E aí pessoal! Hoje estou compartilhando um tutorial sobre como criar um aplicativo web responsivo.",
+      },
+      {
+        type: "image",
+        content: "https://example.com/imagem_tutorial.jpg",
+      },
+      {
+        type: "paragraph",
+        content:
+          "No tutorial, vou mostrar passo a passo como criar um layout incrível usando HTML e CSS.",
+      },
+    ],
+    publishedAt: new Date("2023-04-10 19:23:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -11,7 +64,14 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
--- 01-fundamentos-react/src/components/Post.jsx
+++ 01-fundamentos-react/src/components/Post.jsx
@@ -1,29 +1,56 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
+
+  const publishedDateRelativeToNew = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/RodrigoFerreir4.png" />
+          <Avatar src={author.avatarUrl} />
 
           <div className={styles.authorInfo}>
-            <strong>Rodrigof3rr</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time dateTime="2023-04-10 19:23:00">Publicado há 1h</time>
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNew}
+        </time>
       </header>
       <div className={styles.content}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus
-          similique, corrupti porro unde deleniti iusto doloribus adipisci esse
-          nobis ea aliquam maxime tempora perspiciatis, at earum repudiandae
-          exercitationem et illo.
-        </p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
