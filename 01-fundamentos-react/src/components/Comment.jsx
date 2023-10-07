4cbdbf4a7803a499e0b75d9ff36ec15019c346a2
RodrigoF3rr <rodrigo.4dev@gmail.com>
2023-10-07 16:26:53 -0400

    feat: delete comment

--- 01-fundamentos-react/src/components/Comment.jsx
+++ 01-fundamentos-react/src/components/Comment.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/RodrigoFerreir4.png" />
@@ -16,7 +20,7 @@ export function Comment({ content }) {
               <time dateTime="2023-04-10 19:23:00">Publicado há 1h</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
