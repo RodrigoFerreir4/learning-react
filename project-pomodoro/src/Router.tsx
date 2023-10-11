f48c66e64393968a12aa16372ac6de84977f144e
RodrigoF3rr <rodrigo.4dev@gmail.com>
2023-10-11 15:31:37 -0400

    feat: style home

--- project-pomodoro/src/Router.tsx
+++ project-pomodoro/src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { History } from "./pages/History";
 
-import { DefaultLayout } from "./layouts/DefaultLayout/indedx";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Home } from "./pages/Home";
 
 export function Router() {
