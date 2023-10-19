5738cf2df532445aa2e564c942cc861752d40d0b
RodrigoF3rr <rodrigo.4dev@gmail.com>
2023-10-18 21:54:44 -0400

    feat: create new transaction modal

--- project-finance-management/src/components/Header/index.tsx
+++ project-finance-management/src/components/Header/index.tsx
@@ -1,14 +1,20 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-
+import * as Dialog from "@radix-ui/react-dialog";
 import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+          <NewTransactionModal />
+        </Dialog.Root>{" "}
       </HeaderContent>
     </HeaderContainer>
   );
