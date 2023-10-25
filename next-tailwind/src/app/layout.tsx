dbac72e6add4b96eb4c657dfff34a34a05c55fa8
RodrigoF3rr <rodrigo.4dev@gmail.com>
2023-10-24 23:09:09 -0400

    feat: implement sidebar

--- next-tailwind/src/app/layout.tsx
+++ next-tailwind/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Sidebar } from '@/components/Sidebar'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -16,11 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-    <body className={inter.className}>
+      <body className={inter.className}>
         <div className="grid min-h-screen grid-cols-app">
-          <aside className="border-r border-zinc-200 px-5 py-8">sidebar</aside>
+          <Sidebar />
           <main className="px-4 pb-12 pt-8">{children}</main>
         </div>
-      </body>    </html>
+      </body>
+    </html>
   )
 }
