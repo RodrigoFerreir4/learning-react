455d266fe93f91ea55689aa99b062fd8c9f34a70
RodrigoF3rr <rodrigo.4dev@gmail.com>
2023-10-25 17:52:29 -0400

    feat: styled form

--- next-tailwind/tailwind.config.js
+++ next-tailwind/tailwind.config.js
@@ -6,10 +6,21 @@ module.exports = {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'max-content 1fr max-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
+      },
+
+      borderWidth: {
+        6: '6px',
       },
 
       colors: {
-        diego: '#333',
+        violet: {
+          25: '#fcfaff',
+        },
+      },
+
+      maxWidth: {
+        app: '700px',
       },
     },
   },
